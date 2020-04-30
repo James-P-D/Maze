ea5136a2cc04514d57467872939404303819938f
James Dorrington <jdorrington@hotmail.com>
2020-04-30 13:39:24 +0100

    Initial commit for project

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Maze
 Maze Generation and Solving in Python
+
+## Setup
+
+The program uses the following libraries:
+
+[PyGame](https://www.pygame.org/) Made with v1.9.6
+
+Hopefully `pip` should do the trick...
+
+```
+pip install pygame
+
+```
---
+++ src/MazePy/MazePy/MazePy.py
@@ -0,0 +1,18 @@
+import pygame
+
+def main():
+    pass
+
+###############################################
+# Startup
+###############################################
+
+if __name__ == "__main__":
+    #Single line comment
+    '''
+    Multi-line comment
+    Multi-line comment
+    Multi-line comment
+    '''
+    main()
+
